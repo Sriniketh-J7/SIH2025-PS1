10dfd9834494de91951168796f83f6c7bdf89d08
sifatrajpal <rsifatsingh@gmail.com>
2025-09-08 02:28:36 +0530

    Created SignUp page without backend support

---
+++ frontend/src/Components/InputBox.jsx
@@ -0,0 +1,8 @@
+export const InputBox = ({label,label2}) => {
+    return (
+        <div className="w-full mt-4">
+            <div className="font-bold"> {label} </div>
+            <input type="text" placeholder={label2}  className="border rounded-sm w-full p-2 border-gray-400 mt-2"/>
+        </div>
+    )
+}
