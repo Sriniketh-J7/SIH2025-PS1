40d6b1728465845684b648ee0561f7f447bc71ad
sifatrajpal <rsifatsingh@gmail.com>
2025-09-08 02:30:39 +0530

    Created SignUp page without backend support

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,13 +1,18 @@
-
-
+import { Heading } from "../Components/Heading"
+import { SubHeading } from "../Components/SubHeading"
+import { InputBox } from "../Components/InputBox"
+import { Button } from "../Components/Button"
 export const SignUp = () => {
     return (
         <>
-            <div >
-                <div>
-                    <div>
-                        Sign In 
-                    </div>
+            <div className="h-screen bg-slate-300 flex justify-center items-center ">
+                <div className="w-[30%] h-[70%] bg-white rounded-xl px-5 py-5">
+                    <Heading label="Sign Up" ></Heading>
+                    <SubHeading label="Enter your information to create an account"></SubHeading>
+                    <InputBox label="First Name" label2="nathan"></InputBox>
+                    <InputBox label="Last Name" label2="raw"></InputBox>
+                    <InputBox label="Email" label2="nathanraw@gmail.com"></InputBox>
+                    <Button label="Sign Up"></Button>
                 </div>
             </div>
 
