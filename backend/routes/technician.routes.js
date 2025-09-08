57712e88568c122d0326a5e6ec52fa20231eef89
SreeHarsha2468 <sreeharshavadlamudi2005@gmail.com>
2025-09-09 00:39:01 +0530

    added technicians, alltasks and a single task

---
+++ backend/routes/technician.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authTechnician from "../middlewares/authTechnician.js";
+import {alltasks, task, startTask, updateTask, resolveTask, signuptech, logintech} from "../controllers/technician.controller.js"
+const router = express.Router()
+
+//
+router.post("/signup", signuptech)
+router.post("/login",  logintech)
+router.get("/tasks", authTechnician, alltasks)
+router.get("/tasks/:id", authTechnician, task)
+router.patch("/tasks/:id/start", authTechnician, startTask)
+router.patch("/tasks/:id/update", authTechnician, updateTask)
+router.patch("/tasks/:id/resolve", authTechnician, resolveTask)
+
+
+export default router;
